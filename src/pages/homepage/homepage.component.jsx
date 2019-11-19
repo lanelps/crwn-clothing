db943f05412313855e4b42c6128dca49dbe9665c
Lane le Prevost-Smith <lanelps@gmail.com>
2019-11-19 21:59:07 +1300

    broken hompage into smaller components. Finsihed styling our menu item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+import Directory from "../../components/directory/directory.component";
+
+const Homepage = () => {
+  return (
+    <div className="home-page">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
