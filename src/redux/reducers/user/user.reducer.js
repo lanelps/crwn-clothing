e723232bdbe55f1f8e62cb49506589415469673a
Lane le Prevost-Smith <lanelps@gmail.com>
2019-12-20 10:31:51 +1300

    redirect to home if user is logged in

--- src/redux/reducers/user/user.reducer.js
+++ src/redux/reducers/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "../../actions/user/user.types"
+
 const INITIAL_STATE = {
   currentUser: null
 }
 
 const userReducer = (initialState = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...initialState,
         currentUser: action.payload
