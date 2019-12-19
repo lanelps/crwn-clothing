b8b0bbfad206395d218fc5d49e927ec752f3c107
Lane le Prevost-Smith <lanelps@gmail.com>
2019-12-20 09:57:49 +1300

    added redux and implemented user reducer and user actions to header and app components

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import logger from "redux-logger"
 
 import rootReducer from "./reducers/root-reducer"
+const DEV_TOOLS =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const middleware = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middleware))
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(...middleware), DEV_TOOLS)
+) 
 export default store
